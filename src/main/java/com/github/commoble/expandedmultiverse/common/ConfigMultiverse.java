3cbb2334b3a5a3cb2b27e9ec30f54a04adf8ba01
Commoble <josephbettendorff@yahoo.com>
2018-07-23 19:19:36 -0500

    Changing dimension ID configs now requires MC restart

--- src/main/java/com/github/commoble/expandedmultiverse/common/ConfigMultiverse.java
+++ src/main/java/com/github/commoble/expandedmultiverse/common/ConfigMultiverse.java
@@ -2,9 +2,8 @@ package com.github.commoble.expandedmultiverse.common;
 
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Config.Comment;
-import net.minecraftforge.common.config.Config.LangKey;
 import net.minecraftforge.common.config.Config.RangeInt;
-import net.minecraftforge.common.config.Config.RequiresWorldRestart;
+import net.minecraftforge.common.config.Config.RequiresMcRestart;
 import net.minecraftforge.common.config.Config.Type;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
@@ -26,9 +25,11 @@ public class ConfigMultiverse
 	
 	// TODO consider letting min be 0 and handle appropriately
 	@RangeInt(min=1, max=1000)
+	@RequiresMcRestart
 	@Comment("The number of different worlds that can be accessed via natural wormholes. Changing this will break existing worlds.")
 	public static int perpendicular_universe_count = 97; // largish prime number
 
+	@RequiresMcRestart
 	@Comment("The first ID for perpendicular universe dimensions. The IDs used will form a continuous block of the size of perpendicular_universe_count. Changing this will break existing worlds.")
 	public static int perpendicular_universe_first_ID = 7171117;
 	
